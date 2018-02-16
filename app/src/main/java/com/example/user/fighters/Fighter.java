f4a5e6ce00c87c6f9504bb3c6217e885fc8ae1dc
Bakyt <baktybek2004@mail.ru>
2018-02-16 20:56:13 +0600

    Give name to both fighters

--- app/src/main/java/com/example/user/fighters/Fighter.java
+++ app/src/main/java/com/example/user/fighters/Fighter.java
@@ -5,5 +5,46 @@ package com.example.user.fighters;
  */
 
 public class Fighter {
+    int health;
+    int attact;
+    int defance;
+    String name;
 
+    public Fighter(int attact, int defance, int health, String name) {
+        this.attact = attact;
+        this.defance = defance;
+        this.health = health;
+        this.name = name;
+    }
+
+    public int getAttact() {
+        return attact;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public int getDefance() {
+        return defance;
+    }
+
+    public void setAttact(int attact) {
+        this.attact = attact;
+    }
+
+    public Fighter(){
+        health = 100;
+        attact = 10;
+        defance = 5;
+        name = "Fighter";
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public void setDefance(int defance) {
+        this.defance = defance;
+    }
 }
--- app/src/main/java/com/example/user/fighters/MainActivity.java
+++ app/src/main/java/com/example/user/fighters/MainActivity.java
@@ -2,12 +2,36 @@ package com.example.user.fighters;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    Fighter firstFighter;
+    Fighter secondFighter;
+    Button firstFighterAttack;
+    Button secondFighterAttack;
+    TextView firstFighterName;
+    TextView secondFighterName;
+    TextView winner;
+    TextView action;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        firstFighter = new Fighter(10, 8, 100, "Bruce Lee");
+        secondFighter = new Fighter(15, 15, 100, "Jet Lee");
+
+        firstFighterAttack.setOnClickListener();
+
+
+
+
+
+
     }
 }
